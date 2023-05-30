fafc802eedae9d043b450628195d6aa3cae78eaf
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-05-30 13:55:46 +0500

    Update button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
 export default function Button({ value, span, bg }) {
   return (
     <button type="button" className={`btn ${[span, bg].join('')}`}>{value}</button>
   );
 }
+
+Button.propTypes = {
+  value: PropTypes.string.isRequired,
+  span: PropTypes.string.isRequired,
+  bg: PropTypes.string.isRequired,
+};
