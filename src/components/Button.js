36352900b2346966a459866898c54771f57208fb
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-05-31 14:19:30 +0500

    Add the calculate module

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,11 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
-export default function Button({ value, span, bg }) {
+export default function Button({
+  value, span, bg, press,
+}) {
   return (
-    <button type="button" className={`btn ${[span, bg].join('')}`}>{value}</button>
+    <button type="button" className={`btn ${[span, bg].join('')}`} onClick={press}>{value}</button>
   );
 }
 
@@ -11,4 +13,5 @@ Button.propTypes = {
   value: PropTypes.string.isRequired,
   span: PropTypes.string.isRequired,
   bg: PropTypes.string.isRequired,
+  press: PropTypes.func.isRequired,
 };
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,11 +1,19 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function Screen() {
+export default function Screen({ value }) {
+  const { total, next, operation } = value;
   return (
     <div className="output">
       <div className="current-operand">
-        0
+        {total}
+        {operation}
+        {next}
       </div>
     </div>
   );
 }
+
+Screen.propTypes = {
+  value: PropTypes.string.isRequired,
+};
