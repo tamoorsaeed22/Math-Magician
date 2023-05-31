ea3ed551f8c4e15d451c84a4c2a1a097742fbd51
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-05-31 23:50:04 +0500

    Add quote and Api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Calculator from './components/calculator';
+import FetchQuote from './components/FetchQuote';
 
 function App() {
   return (
     <div className="App">
       <h1>Math Magicians</h1>
       <Calculator />
+      <FetchQuote />
     </div>
   );
 }
